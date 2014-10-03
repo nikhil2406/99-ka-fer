e3e0c79afab3250a20255c2516f3f6f7a0471c28
nikhil2406 <nikhil2406@gmail.com>
2014-10-03 18:03:18 +0530

    new flow according to multi pages for financial plan

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,9 @@ from rango import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^add_user_details/$', views.add_user_details, name='add_user_details'),)
+    url(r'^add_user_details/$', views.add_user_details, name='add_user_details'),
+    url(r'^show_user_details/$', views.show_user_details, name='show_user_details'),
+    url(r'^(?P<user_name_url>\w+)/cash_flow_proj_plan/$', views.cash_flow_proj_plan, name='cash_flow_proj_plan'),
+    url(r'^(?P<user_name_url>\w+)/retirement_plan/$', views.retirement_plan, name='retirement_plan'),
+    url(r'^(?P<user_name_url>\w+)/insurance_plan/$', views.insurance_plan, name='insurance_plan'),)
 
--- rango/views.py
+++ rango/views.py
@@ -12,14 +12,14 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    user_list = UserPersonal.objects.all()
-    context_dict = {'userlist': user_list}
+    #context_dict = {'boldmessage': "I am bold font from the context"}
+    #user_list = UserPersonal.objects.all()
+    #context_dict = {'userlist': user_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('rango/index.html', context_dict, context)
+    return render_to_response('rango/index.html', {}, context)
 
 def add_user_details(request):
     context = RequestContext(request)
@@ -27,12 +27,37 @@ def add_user_details(request):
     if request.method == 'POST':
         form = UserPersonalForm(request.POST)
         if form.is_valid():
-            form.save(commit=True)
-            return index(request)
+            userpersonal = form.save(commit=True)
+            return show_user_details(request,userpersonal.name)
         else:
             print form.errors
     else:
         form = UserPersonalForm()
 
     return render_to_response('rango/add_user_details.html', {'form': form}, context)
+
+
+def show_user_details(request, username):
+    context = RequestContext(request)
+    user = UserPersonal.objects.get(name=username)
+
+    return render_to_response('rango/show_user_details.html',
+        {'username': username, 
+        'userage': user.age,
+        'userphone': user.phone},
+        context)
+
+def cash_flow_proj_plan(request, user_name_url):
+    context = RequestContext(request)
+    return render_to_response('rango/cash_flow_proj_plan.html', {'name': user_name_url}, context)
+
+def retirement_plan(request, user_name_url):
+    context = RequestContext(request)
+    return render_to_response('rango/retirement_plan.html', {'name': user_name_url}, context)
+
+def insurance_plan(request, user_name_url):
+    context = RequestContext(request)
+    return render_to_response('rango/insurance_plan.html', {'name': user_name_url}, context)
+
+
 
